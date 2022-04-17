57beef3746a1f5af092849e40ca42e58237e2c6f
Eduardo Zafra <duduzafra@gmail.com>
2022-04-17 17:04:24 +0200

    Mejorando el codigo aun mas y integrando eventos del teclado con array

--- main.js
+++ main.js
@@ -2,13 +2,30 @@ function tocaSom(idElementAudio) {
   document.querySelector(idElementAudio).play();
 }
 const listaDeTeclas = document.querySelectorAll(".tecla");
-
-let contador = 0;
-while (contador < 9) {
-  const instrumento = listaDeTeclas[contador].classList[1];
+const listaDeTeclasPresionadas = new Array(
+  "KeyU",
+  "KeyI",
+  "KeyO",
+  "KeyJ",
+  "KeyK",
+  "KeyL",
+  "KeyM",
+  "Comma",
+  "Period"
+);
+for (i = 0; i < listaDeTeclas.length; i++) {
+  const teclaPresionada = listaDeTeclasPresionadas[i];
+  const tecla = listaDeTeclas[i];
+  const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`;
-  listaDeTeclas[contador].onclick = function () {
+
+  tecla.addEventListener("click", () => {
     tocaSom(idAudio);
-  };
-  contador = contador + 1;
+  });
+  window.addEventListener("keypress", (e) => {
+    if (e.code == teclaPresionada) {
+      tocaSom(idAudio);
+    }
+  });
+  console.log(i, tecla, teclaPresionada);
 }
