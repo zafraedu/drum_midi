5eb97f4777f5a459b70f885fb3951c4ba96b7bc6
Eduardo Zafra <duduzafra@gmail.com>
2022-04-17 17:01:21 +0200

    Mejorando el codigo con bucles (no hay eventos del teclado)

--- main.js
+++ main.js
@@ -1,104 +1,14 @@
-//funciones
-function tocaSomPom() {
-  document.querySelector("#som_tecla_pom").play();
+function tocaSom(idElementAudio) {
+  document.querySelector(idElementAudio).play();
 }
-function tocaSomClap() {
-  document.querySelector("#som_tecla_clap").play();
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+while (contador < 9) {
+  const instrumento = listaDeTeclas[contador].classList[1];
+  const idAudio = `#som_${instrumento}`;
+  listaDeTeclas[contador].onclick = function () {
+    tocaSom(idAudio);
+  };
+  contador = contador + 1;
 }
-function tocaSomTim() {
-  document.querySelector("#som_tecla_tim").play();
-}
-function tocaSomPuff() {
-  document.querySelector("#som_tecla_puff").play();
-}
-function tocaSomSplash() {
-  document.querySelector("#som_tecla_splash").play();
-}
-function tocaSomToim() {
-  document.querySelector("#som_tecla_toim").play();
-}
-function tocaSomPsh() {
-  document.querySelector("#som_tecla_psh").play();
-}
-function tocaSomTic() {
-  document.querySelector("#som_tecla_tic").play();
-}
-function tocaSomTom() {
-  document.querySelector("#som_tecla_tom").play();
-}
-
-//teclas
-const teclaPom = document.querySelector(".tecla_pom");
-teclaPom.onclick = function () {
-  tocaSomPom();
-};
-const teclaClap = document.querySelector(".tecla_clap");
-teclaClap.onclick = function () {
-  tocaSomClap();
-};
-
-const teclaTim = document.querySelector(".tecla_tim");
-teclaTim.onclick = function () {
-  tocaSomTim();
-};
-
-const teclaPuff = document.querySelector(".tecla_puff");
-teclaPuff.onclick = function () {
-  tocaSomPuff();
-};
-
-const teclaSplash = document.querySelector(".tecla_splash");
-teclaSplash.onclick = function () {
-  tocaSomSplash();
-};
-
-const teclaToim = document.querySelector(".tecla_toim");
-teclaToim.onclick = function () {
-  tocaSomToim();
-};
-
-const teclaPsh = document.querySelector(".tecla_psh");
-teclaPsh.onclick = function () {
-  tocaSomPsh();
-};
-
-const teclaTic = document.querySelector(".tecla_tic");
-teclaTic.onclick = function () {
-  tocaSomTic();
-};
-
-const teclaTom = document.querySelector(".tecla_tom");
-teclaTom.onclick = function () {
-  tocaSomTom();
-};
-
-//teclado
-window.onkeypress = function (e) {
-  if (e.code == "KeyU") {
-    tocaSomPom();
-  }
-  if (e.code == "KeyI") {
-    tocaSomClap();
-  }
-  if (e.code == "KeyO") {
-    tocaSomTim();
-  }
-  if (e.code == "KeyJ") {
-    tocaSomPuff();
-  }
-  if (e.code == "KeyK") {
-    tocaSomSplash();
-  }
-  if (e.code == "KeyL") {
-    tocaSomToim();
-  }
-  if (e.code == "KeyM") {
-    tocaSomPsh();
-  }
-  if (e.code == "Comma") {
-    tocaSomTic();
-  }
-  if (e.code == "Period") {
-    tocaSomTom();
-  }
-};
